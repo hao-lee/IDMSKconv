70a94f1d1803db8b8824b1fdf383d6cc52059572
Hao Lee <haolee.swjtu@gmail.com>
2016-12-14 20:14:57 +0800

    代码全部重构；Python 3.5 移植完成
    1. 原始代码可读性不强，逻辑不清晰，变量名含义很不直观，现在已经全部重构了
    2. 原始代码是在 Python 2.7 环境下写的，现在主流的 Python 版本是 3.5，所以程序内的库函数调用都已经移植到 Python 3.5 环境下

--- IDMSKconv.py
+++ IDMSKconv.py
@@ -1,44 +1,38 @@
 import os,re,sys
 import decomposeTDAs
 def printInfo():
-	print '*'*30
-	print 'IDM Sk Data Extractor'
-	print 'Built data:Dec 6,2009'
-	print 'Author	 :Superfan'
-	print 'Intro	 :This script will extract original data from dictionaries built with IDM Sk'
-	print '			  Tested with LDOCE5 and OALD7'
-	print 'Usage	 :\'python IDMSKconv.py [output path] [dictionary path]\''
-	print 'Example	 :\'python IDMSKconv.py C:\\Output C:\\Dict\''
-	print '			  If no path is specified,it will be defaulted to the current path.'
-	print '*'*30
+	print("Longman Dictionary Data Extractor")
+	print("Version 1.0")
+	print("Developer:")
+
 if __name__=='__main__':
-	#check if options are valid
-	opt = sys.argv[1:]
+	cmd_options = sys.argv[1:]
 	printInfo()
-	if len(opt)>=1:
-		if not os.path.isabs(opt[0]):
-			print '\''+opt[0]+'\''+' is not a valid directory name!'
-			sys.exit()
-		else:outDir=re.match(r'(.*?)\\*$',opt[0]).group(1)
-	else:outDir=os.getcwd()
-	if len(opt)>=2:
-		if not os.path.exists(opt[1]):
-			print '\''+opt[1]+'\''+' doesn\'t exsists!'
-			sys.exit()
-		else:dictDir=re.match(r'(.*?)\\*$',opt[1]).group(1)
-	else:dictDir=os.getcwd()
-	print 'Dictionary dir:',dictDir
-	print 'Output	  dir:',outDir
-	print 'Type any key to begin...'
-	raw_input()
+	output_toplevel_dir=os.getcwd()
+	dict_toplevel_dir=os.getcwd()
+	if len(cmd_options) == 2:
+		output_toplevel_dir = os.path.abspath(cmd_options[0])
+		dict_toplevel_dir = os.path.abspath(cmd_options[1])
+	elif len(cmd_options) == 1:
+		output_toplevel_dir = os.path.abspath(cmd_options[0])
+	else:
+		print("Use Current Directory.")
+	if not (os.path.exists(dict_toplevel_dir) and os.path.exists(output_toplevel_dir)):
+		os._exit(-1)
+	print('Dictionary dir:',dict_toplevel_dir)
+	print('Output	  dir:',output_toplevel_dir)
+	print('Type any key to begin...')
+	#input()
 	#begin to parse dictionary data
-	for root,dirs,files in os.walk(dictDir):
-		if('CONTENT.tda'.lower() in [file.lower() for file in files]):
-			currentOutdir=outDir+re.match(dictDir.replace('\\','\\\\')+r'(.*)',root).group(1)
-			print 'Find target files in:'+'\"'+root+'\"'
-			print 'Current output path:'+currentOutdir
-			if not os.path.exists(currentOutdir):
-				os.makedirs(currentOutdir)
-			decps=decomposeTDAs.decompose(root,currentOutdir)
-			decps.inflateTDA()
-			decps.writeFiles()
+	for current_dir,subdirs,filenames in os.walk(dict_toplevel_dir):
+		# if current directory doesn't contain CONTENT.tda, it isn't the data dir.
+		if('CONTENT.tda'.lower() not in [file.lower() for file in filenames]):
+			continue
+		output_full_dir = os.path.join(output_toplevel_dir, os.path.basename(current_dir))
+		print('This directory contains data files:'+current_dir)
+		print('Now, output to this path:'+output_full_dir)
+		if not os.path.exists(output_full_dir):
+			os.makedirs(output_full_dir)
+		decps=decomposeTDAs.decompose(current_dir,output_full_dir)
+		decps.inflateTDA()
+		decps.writeFiles()
--- decomposeTDAs.py
+++ decomposeTDAs.py
@@ -1,64 +1,72 @@
 import zlib,struct,re
+import os
+
 typeIndex={'ULONG':4,
 	   'U24':3,
 	   'USHORT':2,
 	   'UBYTE':1,
 	   'LINK':0,
 	   'DATA':0}
+
 class decompose:
 	def parseFormat(self):
-	select=0
-		fconfig=open(self.dir+'\\config.cft','rb')
-	for line in fconfig:
-		result = re.match(r'\$(\S+)\s*=\s*(\S+)',line)
-		if result:
-		if result.group(1)=='CONTENT,OFFSET':
+		select=0
+		fconfig=open(os.path.join(self.dir,'config.cft'),'rt')
+		for line in fconfig:
+			result = re.match(r'\$(\S+)\s*=\s*(\S+)',line)
+			if result:
+				if result.group(1)=='CONTENT,OFFSET':
 					self.formatStr[1]=typeIndex[result.group(2)]
 					select=2
-		else:
-			self.formatStr[select]+=typeIndex[result.group(2)]
+				else:
+					self.formatStr[select]+=typeIndex[result.group(2)]
 	def writeOffsetIndex(self):
-	self.offsets=[]
-	i=0
-	fdata=open(self.dir+'\\files.dat','rb')
-	while True:
-		i+=1
-		if(len(fdata.read(self.formatStr[0]))==0):break
-		self.offsets.append(struct.unpack('L',str(fdata.read(self.formatStr[1])+(4-self.formatStr[1])*'\x00'))[0])
-		fdata.read(self.formatStr[2])
+		self.offsets=[]
+		i=0
+		fdata=open(os.path.join(self.dir,'files.dat'),'rb')
+		while True:
+			i+=1
+			if(len(fdata.read(self.formatStr[0]))==0):
+				break
+			bytes_object = fdata.read(self.formatStr[1])
+			for i in range(4 - self.formatStr[1]):
+				bytes_object += b'\x00'
+			self.offsets.append(struct.unpack('L',bytes_object)[0])
+			fdata.read(self.formatStr[2])
 	def inflateTDA(self):
-		fin=open(self.dir+'\\CONTENT.tda', 'rb')
-		fdst=open(self.outdir+'\\output', 'wb')
-		findex=open(self.dir+'\\CONTENT.tda.tdz','rb')
+		fin=open(os.path.join(self.dir,'CONTENT.tda'), 'rb')
+		fdst=open(os.path.join(self.outdir,'output'), 'wb')
+		findex=open(os.path.join(self.dir,'CONTENT.tda.tdz'),'rb')
 		byte=[]
 		while True:
 			bin=findex.read(8)
 			if len(bin)==0:break
-			byte.append(struct.unpack('ii',str(bin)))
+			byte.append(struct.unpack('ii',bin))
 		i=0
-		print 'Now decompressing...'
+		print('Now decompressing...')
 		for xi,bytei in byte:
 			i+=1
 			dedata=zlib.decompress(fin.read(bytei))
 			fdst.write(dedata)
-		print 'Done!Total %d entries.' %i
+		print('Done!Total %d entries.' %i)
 	def writeFiles(self):
-		fin=open(self.dir+'\\NAME.tda','rb')
+		fin=open(os.path.join(self.dir,'NAME.tda'),'rb')
 		raw=fin.read()
-		name=raw.split('\x00')
-		import os
-		fin=open(self.outdir+'\\output','rb')
+		name_bytes_obj_list=raw.split(b'\x00')
+		fin=open(os.path.join(self.outdir,'output'),'rb')
 		for i in range(len(self.offsets)):
-			fout=open(self.outdir+'\\'+name[i],'wb')
+			name_bytes_obj = name_bytes_obj_list[i]
+			name = name_bytes_obj.decode('utf-8')
+			fout=open(os.path.join(self.outdir,name),'wb')
 			if(i==len(self.offsets)-1):
 				fout.write(fin.read()[:-1])
 			else:
 				fout.write(fin.read(self.offsets[i+1]-self.offsets[i])[:-1])
-			print 'Now writing separate files:'+str(i+1)+'\r',
-		print '\nDone!'
+			print('Now writing separate files:'+str(i+1)+'\r',)
+		print('\nDone!')
 	def __init__(self,dir,outdir):
-	self.dir=dir
+		self.dir=dir
 		self.outdir=outdir
-	self.formatStr=[0,0,0]
-	self.parseFormat()
+		self.formatStr=[0,0,0]
+		self.parseFormat()
 		self.writeOffsetIndex()
